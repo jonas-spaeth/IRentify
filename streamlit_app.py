9a7c3493fd14a44ec58f2fd68cb0ea9525144d55
ru35hub <jonas.spaeth@campus.lmu.de>
2023-06-05 20:18:03 +0200

    add xls

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,27 @@ matplotlib.use('Agg')
 # Test: Linechart from a dataframe
 
 
-st.header("Druckdiagramme Bayern")
+st.header("IR Spectra Identify")
 
-label = 'Passwort eingeben um Diagramme zu sehen.'
+label = 'Tolerance?'
+peaks = "0"
+peaks = st.text_input('tolerance')
+peaks = [float(p) for p in peaks.split(',')]
 
-st.markdown("---")
+st.write(peaks)
 
-st.subheader(":dash:  Föhn über den Alpen")
+
+df = pd.read_excel("Wellenzahlen-Tool.xls")[["zahl", "stoff"]]
+tolerance = 4
+potentials = []
+for p in peaks[:]:
+    diff = df["zahl"] - p
+    df_sorted = df.iloc[diff.abs().argsort()]
+    foo = df_sorted[(df_sorted["zahl"] - p).abs() < tolerance]
+    potentials += foo["stoff"].to_list()
+counts = pd.Series(potentials).groupby(potentials).count()
+counts_sorted = counts.sort_values(ascending=False)
+
+st.write(counts_sorted[counts_sorted > 2])
 
 st.markdown("---")
