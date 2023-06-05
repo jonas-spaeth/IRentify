9a8d0e3c5a481403ed89054a153853174f748290
ru35hub <jonas.spaeth@campus.lmu.de>
2023-06-05 20:09:03 +0200

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import matplotlib
+
+matplotlib.use('Agg')
+
+# Test: Linechart from a dataframe
+
+
+st.header("Druckdiagramme Bayern")
+
+label = 'Passwort eingeben um Diagramme zu sehen.'
+
+st.markdown("---")
+
+st.subheader(":dash:  Föhn über den Alpen")
+
+st.markdown("---")
