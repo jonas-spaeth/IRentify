29676ebb5124f5c05d0094651f2654e79c10cec9
ru35hub <jonas.spaeth@campus.lmu.de>
2023-06-05 20:32:57 +0200

    add plot, strip peaks

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import matplotlib
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 matplotlib.use('Agg')
 
@@ -10,15 +12,19 @@ matplotlib.use('Agg')
 st.header("IR Spectra Identify")
 
 label = 'Tolerance?'
+tolerance = st.number_input('tolerance (must be larger 0', min_value=1, value=5)
+
 peaks = "0"
 peaks = st.text_input('tolerance')
-peaks = [float(p) for p in peaks.split(',')]
+if len(peaks) > 0:
+    peaks = [float(p.strip()) for p in peaks.split(',')]
 
-st.write(peaks)
+    st.subheader("Your measured peaks:")
+    st.write(peaks)
 
+st.subheader("Result")
 
 df = pd.read_excel("Wellenzahlen-Tool.xls")[["zahl", "stoff"]]
-tolerance = 4
 potentials = []
 for p in peaks[:]:
     diff = df["zahl"] - p
@@ -31,3 +37,19 @@ counts_sorted = counts.sort_values(ascending=False)
 st.write(counts_sorted[counts_sorted > 2])
 
 st.markdown("---")
+
+
+import matplotlib.pyplot as plt
+
+analyze_drug = counts_sorted[counts_sorted > 2].index[0]
+
+
+fig = plt.figure(figsize=(20, 5))
+theory = df[df["stoff"] == analyze_drug].zahl
+for t in theory:
+    plt.axvline(t, c="k", lw=5)
+
+for p in peaks:
+    plt.axvline(p, c="r", lw=2, ls="--")
+plt.title(f"Red: measured, Black: {analyze_drug}")
+fig.show()
